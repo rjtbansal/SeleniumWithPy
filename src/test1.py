f44de391d8872147cc78451c804d37d34e3beb8e
rjtbansal <rjtbansal@yahoo.com>
2017-04-10 17:08:27 -0700

    first simple script in selenium

---
+++ src/test1.py
@@ -0,0 +1,6 @@
+#first test script
+from selenium import webdriver
+#need to specify path to Chrome
+driver=webdriver.Chrome("C:\Python27\Scripts\chromedriver")
+#opening facebook.com in browser to test
+driver.get("http://www.facebook.com")
