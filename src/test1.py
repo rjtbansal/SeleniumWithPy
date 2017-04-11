b366e4ad406a50f8b595aaa02817a21790ff720f
rjtbansal <rjtbansal@yahoo.com>
2017-04-10 17:45:57 -0700

    Added firefox

--- src/test1.py
+++ src/test1.py
@@ -1,7 +1,13 @@
 #first test script
 from selenium import webdriver
 #need to specify path to Chrome
-driver=webdriver.Chrome("C:\Python27\Scripts\chromedriver")
+driver_chrome=webdriver.Chrome("C:\Python27\Scripts\chromedriver")
+
+#using firefox..notice geckodriver path is specified..here unlike chromedriver we dont have to specify the whole path for firefox
+#so a path like C:\Python27\Scripts\geckodriver will throw not found error
+driver_firefox=webdriver.Firefox("C:\Python27\Scripts")
 #opening facebook.com in browser to test
-driver.get("http://www.facebook.com")
+driver_chrome.get("http://www.facebook.com") #opening facebook in chrome
+driver_firefox.get("http://www.cricinfo.com") #opening cricinfo in firefox
+
 #end
