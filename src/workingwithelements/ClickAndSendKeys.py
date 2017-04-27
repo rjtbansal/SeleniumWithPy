ff68c03f4893404a21c56619f2b8a8aa4cf77c7e
rjtbansal <rjtbansal@yahoo.com>
2017-04-26 22:23:29 -0700

    element state

---
+++ src/workingwithelements/ClickAndSendKeys.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+class ClickAndSendKeys():   
+    driver=webdriver.Chrome()
+    #driver = webdriver.Firefox() #somehow the firefox version I had didnt support send_keys and I ended up getting string undefined error
+    
+    def getUrl(self, base_url):
+        self.driver.get(base_url)
+        self.driver.maximize_window() #safer to maximize the window..sometimes the elements we want to work on arent visible and we get an exception Element Not Visible
+        
+    def test(self):
+        #click on login         
+        login_button = self.driver.find_element(By.XPATH, "//div[@id='navbar']//a[@href='/sign_in']")
+        login_button.click() #finding element and then clicking
+        
+        self.driver.implicitly_wait(5) #waiting 5 seconds for the next page to load..applies to all elements that will take time to load
+        
+        time.sleep(5) #sleeping for 5 seconds..just to see the automated typing
+        email_field=self.driver.find_element(By.ID, "user_email")
+        email_field.send_keys("rjtbansal") #sending the keys in input field email
+        
+        password_field=self.driver.find_element(By.ID, "user_password")
+        password_field.send_keys("rajat")
+        
+        email_field.clear() #clearing the input field
+        time.sleep(5)
+        email_field.send_keys('rjtbansal')
+        
+clickSend = ClickAndSendKeys()
+clickSend.getUrl("https://letskodeit.teachable.com/p/practice")
+clickSend.test()        
+        
+        
---
+++ src/workingwithelements/ElementState.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+
+class ElementState():
+    driver = webdriver.Firefox()
+    
+    def getUrl(self, base_url):
+        self.driver.get(base_url)
+        self.driver.maximize_window()
+    
+    def testState(self):
+        self.driver.implicitly_wait(5)
+        #many times there are elements pointing to the same location and some are disabled..so in that scenario we wont be able to
+        #do normal operations like send_keys for input type element
+        search_elem = self.driver.find_element_by_id("gs_htif0")
+        search_elem_state = search_elem.is_enabled() #checking if element is enabled
+        if search_elem_state: #if it is only then send_keys
+            search_elem.send_keys("angularjs tutorial  --- 1")
+        
+        search_elem = self.driver.find_element_by_id("gs_taif0")
+        search_elem_state = search_elem.is_enabled()
+        if search_elem_state:
+            search_elem.send_keys("angularjs tutorial --- 2")
+            
+        search_elem = self.driver.find_element_by_id("lst-ib")
+        search_elem_state = search_elem.is_enabled()
+        if search_elem_state:
+            search_elem.send_keys("angularjs tutorial --- 3")
+        
+es = ElementState()
+es.getUrl("http://www.google.com")
+es.testState()
